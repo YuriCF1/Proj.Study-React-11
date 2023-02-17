0eace35aaa39abcc6ca685aa56c4867db872e31b
Yuri <yurifdev@gmail.com>
2023-02-17 15:27:01 -0300

    useEffect with dependency on array

--- reacthooks/src/Components/HookUseEffect.js
+++ reacthooks/src/Components/HookUseEffect.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 const HookUseEffect = () => {
   // 5 - useEffect sem dependências
   useEffect(() => {
-    console.log("Estou sendo executado");
+    console.log("Sou executado toda vez que um componente renderiza");
   }); // Sem array = Toda vez que o componente for executado
 
   const [number, setNumber] = useState(1);
@@ -14,14 +14,31 @@ const HookUseEffect = () => {
 
   // 5 - useEffect com dependências vazia
   useEffect(() => {
-    console.log("Serei executado, apena uma vez");
+    console.log("Serei executado, apena uma vez. Quando a página carregar");
   }, []); // Com array = toda vez que a página carrega. Ex: Trazer dados do banco
+
+  // 6 - Item com array de dependências
+  const [anotherNumber, setAnotherNumber] = useState(0);
+
+  useEffect(() => {
+    if (anotherNumber > 0) { //Valindando caso o dado não exista ainda
+      console.log("Another number mudou. Agora é: ", anotherNumber);
+    }
+  }, [anotherNumber]);
 
   return (
     <div>
       <h2>useEffect</h2>
       <p>Number: {number}</p>
-      <button onClick={changeNumber}>Executar +1</button>
+      <p>Number: {number}</p>
+      <button onClick={changeNumber}>Number +1</button>
+      <button
+        onClick={() => {
+          setAnotherNumber(anotherNumber + 1);
+        }}
+      >
+        AnotherNumber +1
+      </button>
       <hr />
     </div>
   );
