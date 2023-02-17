73029367191c06142ca61a93cd4743d763f9b38e
Yuri <yurifdev@gmail.com>
2023-02-17 18:43:12 -0300

    Creating the context

---
+++ reacthooks/src/Components/HookUseContext.js
@@ -0,0 +1,12 @@
+ // 8 - useContext 
+import { createContext } from "react";
+
+export const SomeContext = createContext();
+ 
+export const HookUseContext = ({ children }) => {
+  const contextValue = "Text Context";
+
+  return (
+    <SomeContext.Provider value={{contextValue}}>{children}</SomeContext.Provider>
+  );
+};
