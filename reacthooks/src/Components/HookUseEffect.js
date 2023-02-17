45008a29153b3fa8d647e3842ac1a2088fa9495b
Yuri <yurifdev@gmail.com>
2023-02-17 15:10:33 -0300

    useEffect with an empty depency array

--- reacthooks/src/Components/HookUseEffect.js
+++ reacthooks/src/Components/HookUseEffect.js
@@ -4,13 +4,18 @@ const HookUseEffect = () => {
   // 5 - useEffect sem dependências
   useEffect(() => {
     console.log("Estou sendo executado");
-  }); // Sem array = Toda vez que o componente for executado 
+  }); // Sem array = Toda vez que o componente for executado
 
-  const [number, setNumber] = useState(1)
+  const [number, setNumber] = useState(1);
 
   const changeNumber = () => {
-    setNumber(number + 1)
-  }
+    setNumber(number + 1);
+  };
+
+  // 5 - useEffect com dependências vazia
+  useEffect(() => {
+    console.log("Serei executado, apena uma vez");
+  }, []); // Com array = toda vez que a página carrega. Ex: Trazer dados do banco
 
   return (
     <div>
