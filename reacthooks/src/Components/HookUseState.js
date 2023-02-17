ac6a017d002e6c4b88a1299419b93817145ac14d
Yuri <yurifdev@gmail.com>
2023-02-16 21:55:02 -0300

    useState com inputs

--- reacthooks/src/Components/HookUseState.js
+++ reacthooks/src/Components/HookUseState.js
@@ -10,6 +10,16 @@ const HookUseState = () => {
 
     console.log(userName, name);
   };
+
+  // 2 -useState e inputs
+  const [age, setAge] = useState(18);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //envio da API
+    console.log(age);
+  };
   return (
     <div>
       {/* 1 - useState */}
@@ -17,6 +27,19 @@ const HookUseState = () => {
       <p>Variável: {userName}</p>
       <p>useState: {name}</p>
       <button onClick={changeNames}>Mudar nomes</button>
+      <hr />
+      {/* 2 - useState input */}
+      <p>Digite sua idade</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />{" "}
+        {/*Aderindo o state ao value para poder alterar com o state*/}
+      </form>
+      <p>Você tem {age} anos</p>
+      <button type="submit" value="Enviar"></button>
       <hr />
     </div>
   );
