ad6cc21c4dc8063ee5bb82fb3b2982122c8f975b
Yuri <yurifdev@gmail.com>
2023-02-21 17:12:24 -0300

    Fixing custom hook

--- reacthooks/src/Hooks/usePrevious.js
+++ reacthooks/src/Hooks/usePrevious.js
@@ -2,8 +2,8 @@ import { useEffect, useLayoutEffect, useRef } from "react";
 
 export const usePrevious = (value) => {
   const ref = useRef(0);
-  useLayoutEffect(() => {
-    ref.current = value; //Salva o valor novo
-  }); 
+  useEffect(() => {
+    ref.current = value; //Salva o valor novo 
+  }, []); 
   return ref.current; //Devolve o valor antigo
 };
