ed06d09f901ffe7fe861eb51211836fd3778bffa
Yuri <yurifdev@gmail.com>
2023-02-21 17:28:10 -0300

    using the debug hook with the extension in the browser

--- reacthooks/src/Hooks/usePrevious.js
+++ reacthooks/src/Hooks/usePrevious.js
@@ -1,9 +1,12 @@
-import { useEffect, useLayoutEffect, useRef } from "react";
+import { useDebugValue, useEffect, useLayoutEffect, useRef } from "react";
 
 export const usePrevious = (value) => {
   const ref = useRef(0);
+
+  useDebugValue("--- CUSTOM HOOK E USEDEBUG VALUE---")
+  useDebugValue("O número anterior é: ", value)
   useEffect(() => {
     ref.current = value; //Salva o valor novo 
-  }, []); 
+  }); 
   return ref.current; //Devolve o valor antigo
 };
